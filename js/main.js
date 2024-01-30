c80b21af4fff55e2bc49d5daa14e3879cce3c7e3
miaaae <miaaaaae@gmail.com>
2024-01-30 19:15:39 +0900

    header 수정

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 
     // mobile menu
     $('.mo-btn').click(function(){
-        $('.main-menu-wrap').addClass('on');;        
+        $('.main-menu-wrap.mo').addClass('on');        
         $('.dimmed').show();
         $('body').addClass('stop-scroll');
     });
 
     $('.main-menu-wrap .menu-close .btn-close').click(function(){
-        $('.main-menu-wrap').removeClass('on');
+        $('.main-menu-wrap.mo').removeClass('on');
         $('.dimmed').hide();
         $('body').removeClass('stop-scroll');
 
